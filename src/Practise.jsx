2b25d5df69c83fba091ab0f6e2fb9f39196d8d44
sim994 <hirenpipaliya418@gmail.com>
2026-03-28 12:49:16 +0530

    ScrollTriger Practise

--- src/Practise.jsx
+++ src/Practise.jsx
@@ -1,6 +1,7 @@
-import React, { useRef } from "react";
+import React, { useRef, useLayoutEffect } from "react";
 import { useGSAP } from "@gsap/react";
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export function Practise() {
   // useRef Practise
@@ -18,29 +19,54 @@ export function Practise() {
   // })
 
   // TimeLine Practise
-  let tl = gsap.timeline();
+  // let tl = gsap.timeline();
 
-  tl.to(".box", {
-    x: 800,
-    duration: 2,
-    delay: 1,
-    rotate: 360,
-    borderRadius: 300,
-    backgroundColor: "blue",
-  });
-  tl.to(".box1", {
-    x: 800,
-    duration: 2,
-    delay: 1.5,
-    rotate: 360,
-    borderRadius: 300,
-    backgroundColor: "blue",
-  });
+  // tl.to(".box", {
+  //   x: 800,
+  //   duration: 2,
+  //   delay: 1,
+  //   rotate: 360,
+  //   borderRadius: 300,
+  //   backgroundColor: "blue",
+  // });
+  // tl.to(".box1", {
+  //   x: 800,
+  //   duration: 2,
+  //   delay: 1.5,
+  //   rotate: 360,
+  //   borderRadius: 300,
+  //   backgroundColor: "blue",
+  // });
+
+  // ScrollTriger Practise
+  gsap.registerPlugin(ScrollTrigger);
+
+  useLayoutEffect(() => {
+    gsap.to("h1", {
+      transform: "translateX(-100%)",
+      scrollTrigger: {
+          trigger:"h1",
+          top:"top center",
+          end:"top 20%",
+          markers:true,
+          pin:true,
+          scrub:1,
+      },
+    });
+  }, []);
+
   return (
     <section className="practise">
       {/* <div className="box" ref={boxref}></div> */}
-      <div className="box"></div>
-      <div className="box1"></div>
+      {/* <div className="box"></div>
+      <div className="box1"></div> */}
+      <section className="sec1">
+      </section>
+      <section className="sec">
+        <h1>ScrollTriger Animation</h1>
+      </section>
+      <section className="sec1">
+      </section>
     </section>
   );
 }
