334a120efa5c249bd38707bdcc5139c1835858d3
Hiren prajapati <hirenpipaliya418@gmail.com>
2026-03-28 11:04:36 +0530

    Delete gsap directory

--- gsap/src/About.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function About() {
-  return (
-    <section className="about-page">
-      <div><h1>This Is a About pages</h1></div>
-    </section>
-  );
-}
--- gsap/src/main.jsx
+++
@@ -1,5 +0,0 @@
-import { render } from 'preact'
-import './index.css'
-import './Demo.css'
-import { Page } from "./Page";
-render(<Page />, document.getElementById('app'))
