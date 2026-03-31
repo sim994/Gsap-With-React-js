ca4c52b25ce989d28dfad996d994dc64b3b155a7
sim994 <hirenpipaliya418@gmail.com>
2026-03-31 11:30:46 +0530

    Stagger Animation Demo

---
+++ src/Stagger.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+
+export function Stagger() {
+    useGSAP(()=>{
+        gsap.from(".skills h3",{
+            opacity:0,
+            scale:0,
+            duration:1,
+            filter:"blur(10px)",
+            stagger:{
+                grid:[3,3],
+                from:"right",
+                axis:"x",
+                amount:.5
+            }
+        })
+    })
+  return (
+    <section className="stagger">
+      <div className="wrapper">
+        <h1>Professional Skills</h1>
+        <div className="skills">
+          <h3>Web Design</h3>
+          <h3>Video Editing</h3>
+          <h3>Content Creator</h3>
+          <h3>Logo Designer</h3>
+          <h3>Seo</h3>
+          <h3>Communication</h3>
+          <h3>Leadership</h3>
+          <h3>Time Management</h3>
+        </div>
+      </div>
+    </section>
+  );
+}
