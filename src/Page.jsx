c2c4ea373ef7c6dd3328e8a3f884ddffc4afa51c
sim994 <hirenpipaliya418@gmail.com>
2026-04-01 12:46:40 +0530

    updated

--- src/Page.jsx
+++ src/Page.jsx
@@ -4,6 +4,9 @@ import { HeroPage } from "../src/HeroPage";
 import { About } from "../src/About";
 import { Practise } from "../src/Practise";
 import { Stagger } from "../src/Stagger";
+import { Random } from "../src/Random"; 
+import { StaggerEffect } from "../src/StaggerEffect";
+import { SvgEffect } from "../src/SvgEffect";
 import { ScrollEffects} from "../src/ScrollEffects";
 
 export function Page() {
