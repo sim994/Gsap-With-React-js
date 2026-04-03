b238f638a8967ab0aea9896c1e0785bf52304858
sim994 <hirenpipaliya418@gmail.com>
2026-04-03 11:18:29 +0530

    Animation Completed

--- src/CardAnimation.jsx
+++ src/CardAnimation.jsx
@@ -1,32 +1,34 @@
-import React from 'react'
+import React, { useLayoutEffect } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export function CardAnimation() {
   gsap.registerPlugin(ScrollTrigger);
-  const tl = gsap.timeline()
+  const tl = gsap.timeline();
 
-  gsap.utils.toArray(".card").forEach((card) => {
-    tl.to(card,{
-      scale: .5,
-      opacity: 0,
-      scrollTrigger: {
-        trigger: card,
-        start: "20% 50%",
-        end: "top 30%",
-        scrub: 1,
-        markers: true,
-      }
-    })
-  })
+  useLayoutEffect(() => {
+    gsap.utils.toArray(".card").forEach((card) => {
+      gsap.to(card, {
+        scale: 0.5,
+        opacity: 0,
+        scrollTrigger: {
+          trigger: card,
+          start: "top 15%",
+          end: "bottom 15%",
+          scrub: 1,
+          markers: true,
+        },
+      });
+    });
+  });
+
   return (
     <section className="card-section">
-        <div className="card">1</div>
-        <div className="card">2</div>
-        <div className="card">3</div>
-        <div className="card">4</div>
-        <div className="card">5</div>
+      <div className="card">1</div>
+      <div className="card">2</div>
+      <div className="card">3</div>
+      <div className="card">4</div>
+      <div className="card">5</div>
     </section>
-  )
+  );
 }
-
